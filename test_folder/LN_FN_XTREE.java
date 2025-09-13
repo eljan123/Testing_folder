732faa1bd88a62bfc6bd69d4fe9f878b4aa903d7
eljanpamintuan <eypamintuan.work@gmail.com>
2025-09-13 13:47:04 +0800

    basic print exercise

---
+++ test_folder/LN_FN_XTREE.java
@@ -0,0 +1,24 @@
+public class LN_FN_XTREE {
+    public static void main(String[] args) {
+        System.out.println("         *         ");
+        System.out.println("        ***        ");
+        System.out.println("       *@*@*       ");
+        System.out.println("      **%*%**      ");
+        System.out.println("     ***$@$***     ");
+        System.out.println("    **@**%**@**    ");
+        System.out.println("   ***#**@**#***   ");
+        System.out.println("  ****@**%**@****  ");
+        System.out.println(" *****%**@**%***** ");
+        System.out.println("******#**%**#******");
+        System.out.println("      ||||||      ");
+        System.out.println("     [||||||]     ");
+        System.out.println("     [||||||]    ");
+        System.out.println("     [||||||]   ");
+        System.out.println("     [||||||]  ");
+        System.out.println("    {~~~~~~~~} ");
+        System.out.println("   {~~~~~~~~~~}");
+        System.out.println("      ######   ");
+        System.out.println("     ########  ");
+        System.out.println("    ########## ");
+    }
+}
